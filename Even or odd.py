2ae24a27490a87eaf601bbece43a187ae6af8614
Stirkey <tirkeysahil28@gmail.com>
2026-05-17 17:27:36 +0530

    even or odd

---
+++ Even or odd.py
@@ -0,0 +1,9 @@
+#Check even or odd function
+def check_even_odd(num):
+    if num % 2 == 0:
+        print(num, "is Even")
+    else:
+        print(num, "is Odd")
+
+check_even_odd(10)
+check_even_odd(7)
